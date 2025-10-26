6b95bc4539cc97e10a290c9332ee2f77465548d7
gupipipipi <gupippipi@gmail.com>
2025-10-26 21:43:31 +0900

    Delete Takeyui_G1 directory

--- Takeyui_G1/frontend/src/register_items.js
+++
@@ -1,6 +0,0 @@
-// register.js
-import { createApp } from 'vue';
-import Register_items from './Register_items.vue';
-
-// アプリケーションインスタンスを作成し、HTMLの#register-app要素にマウント
-createApp(Register_items).mount('#register_items-app');
